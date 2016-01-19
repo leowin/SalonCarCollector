0e4146254f57bfc4766ba8f6d7cf7c4772dcc7ce
leowin <lw@zentriert.at>
2016-01-19 12:43:58 +0100

    startup

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,26 @@
+var texts= {
+	"fail": {"de-CH": "Fehler beim herunterladen der Inhalte. Bitte überprüfen Sie die Internetverbindung!",
+			 "fr-CH": "Fehler beim herunterladen der Inhalte. Bitte überprüfen Sie die Internetverbindung!",
+			 "it-CH": "Fehler beim herunterladen der Inhalte. Bitte überprüfen Sie die Internetverbindung!"},
+	"sync": {"de-CH": "Lade Inhalte...",
+			 "fr-CH": "Lade Inhalte...",
+			 "it-CH": "Lade Inhalte..."},
+	"configerr": {"de-CH": "configerr",
+			 "fr-CH": "configerr",
+			 "it-CH": "configerr"},
+			
+}
+
 var app = {
     // Application Constructor
+	lang: "",
+	
     initialize: function() {
+		this.lang = navigator.language;
+		if (this.lang == "de" || this.lang == "it" || this.lang == "fr")
+			this.lang = this.lang + "-CH";			
+		if( this.lang != "de-CH" && this.lang != "it-CH" && this.lang != "fr-CH")
+			this.lang = "fr-CH";
         this.bindEvents();
     },
     // Bind Event Listeners
@@ -9,44 +29,63 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
-		document.getElementById( 'gobutton').addEventListener('click', this.navigate);
-		document.getElementById( 'seturlbutton').addEventListener('click', this.setUrl);
-		document.getElementById( 'syncbutton').addEventListener('click', this.sync);
-
-		document.getElementById( 'newDocumentCid').addEventListener('click', this.newDocumentCid);
-		document.getElementById( 'saveDocument').addEventListener('click', this.saveDocument);
-		document.getElementById( 'getLastSync').addEventListener('click', this.getLastSync);
-		document.getElementById( 'deleteDocument').addEventListener('click', this.deleteDocument);
-		document.getElementById( 'getContentRootUri').addEventListener('click', this.getContentRootUri);
-		document.getElementById( 'searchDocuments').addEventListener('click', this.searchDocuments);
-		document.getElementById( 'getDocumentCount').addEventListener('click', this.getDocumentCount);
-		},
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
+	},
+    
     onDeviceReady: function() {
 		cordova.plugins.DCSync.on('sync_completed', app.synccompleted);
 		cordova.plugins.DCSync.on('sync_failed', app.syncfail);
 		cordova.plugins.DCSync.on('sync_progress', app.syncprogress);
-		cordova.plugins.DCSync.getSyncOptions().then(function(s) {
-			document.getElementById('url').value = s.url;
-		});
-		navigator.splashscreen.hide();
-		app.checkStartPage();
+		cordova.plugins.DCSync.getLastSync().then(function(date) {
+			console.log('lastSync:' + JSON.stringify(date));
+			if( date && date.syncDate && date.syncDate > new Date().getTime() - 1000*60*60*24*5) {
+				app.checkStartPage();
+			}
+			else {
+				app.initSync();
+				//force sync first time or after 5 days not synced
+			}
+		}, app.failure);
     },
-
+	initSync: function() {
+		cordova.plugins.DCSync.setSyncOptions({url:'https://ch-co2tieferlegen.preview.kju.at/DC', username:'anonymous', password:'YTI2Y2UxYzgxZTBiN2U4OWZmZjU1OWJmYmU4ZTEwN2E2MzFhNGFlMmFkYzlhMWMxYmE1YWYyOGNiYWMyZWI4ZA==', interval: 1440})
+			.then(function() {
+				console.log('syncOptionsSet:');
+				startSync();
+			}, app.configerr);
+	},
+	startSync: function() {
+		app.toast("sync");
+		cordova.plugins.DCSync.performSync().then(function() {
+			console.log('sync start requested:');
+		}, app.configerr)
+	},
+	configerr: function() {
+		app.toast("configerr");
+		app.exit();
+	},
+	exit: function() {
+		navigator.app.exitApp();
+	},
+	toast: function(identifier, length) {
+		window.plugins.toast.show(getString(identifier), length ? length : "long", 'bottom', function(a){console.log('toast success: ' + a)}, function(b){alert('toast error: ' + b)});
+	},
 	syncprogress: function(pro) {
-		document.getElementById('syncprogress').innerHTML= pro.percent + "Sync %...";
+		
 	},
 	synccompleted: function(pro) {
-		document.getElementById('syncprogress').innerHTML= "Sync complete";
+		console.log('sync completed:');
 		app.checkStartPage();
 	},
-	syncfail: function(pro) {
-		document.getElementById('syncprogress').innerHTML= "Sync failed" + JSON.stringify(pro);
+	syncfail: function(a) {
+		console.log('sync failed: ' + JSON.stringify(a));
+		toast("fail");
+		//retry after timeout
+		window.timeout(10000, app.startSync );
 	},
-
+	getString(identifier) {
+		var t =texts[identifier]
+		return t[app.lang];
+	},
 	setUrl: function() {
 		var url = document.getElementById('url').value;
 		var interval = document.getElementById('interval').value *1;
@@ -62,80 +101,30 @@ var app = {
 
 	navigate: function() {
 		app.clear();
-		var url = document.getElementById('startpage').innerHTML + "?assets=" + encodeURI(cordova.file.applicationDirectory);
-		window.location=url;
+		
 	},
 	
-	clear: function() {
-		document.getElementById('result').innerHTML = "";
-		document.getElementById('error').innerHTML= "";
-	},
-	
-	log: function(data) {
-		document.getElementById('result').innerHTML= "complete: <br/>" + (!!data ? JSON.stringify(data) : "");
-	},
-	failure: function(data) {
-		document.getElementById('error').innerHTML= (!!data ? JSON.stringify(data) : "");
-	},
 	
 	checkStartPage: function() {
-		var ele = document.getElementById('startpage');
-		cordova.plugins.DCSync.searchDocuments( {}, {path:"co2tl_app/index.html"}).then( function(data) {
+		return  cordova.plugins.DCSync.searchDocuments( {}, {path:"co2tl_app/index.html"}).then( function(data) {
+				console.log('start page: ' + JSON.stringify(data));
 				if( data && data.length>0 && data[0].files && data[0].files.length>0 ) {
-					cordova.plugins.DCSync.getContentRootUri().then( function(uri) {
-						ele.innerHTML = uri.replace("cdvfile://localhost/", "file:///") + data[0].files[0];
-						document.getElementById('gobutton').style="";
-						app.navigate();
-					}).fail(app.failure);
+					return	cordova.plugins.DCSync.getContentRootUri().then( function(uri) {
+						console.log('content root: ' + JSON.stringify(uri));
+						if (window.resolveLocalFileSystemURL) {
+							window.resolveLocalFileSystemURL(fr, function (entry) {
+								var url = entry.toURL() + data[0].files[0] + "?assets=" + encodeURI(cordova.file.applicationDirectory);
+								console.log('start page url root: ' + JSON.stringify(url));
+								//todo handle errors
+								window.location=url;
+							});
+						}
+					}).fail(app.configerr);
 				}
-				else {
-					ele.innerHTML = "No Startpage synced yet";
-					document.getElementById('gobutton').style="display:none";
-				}
-		}).fail(app.failure);
+				//no startpage present -> config error;
+				app.configerr();
+		}).fail(app.configerr);
 	},
-	
-	newDocumentCid: function() {
-		app.clear();
-		cordova.plugins.DCSync.newDocumentCid().then(app.log, app.failure);
-	},
-
-	getLastSync: function() {
-		app.clear();
-		cordova.plugins.DCSync.getLastSync().then(app.log, app.failure);
-	},
-
-	getContentRootUri: function() {
-		app.clear();
-		cordova.plugins.DCSync.getContentRootUri().then(app.log, app.failure);
-	},
-
-	saveDocument: function() {
-		app.clear();
-		cordova.plugins.DCSync.saveDocument('TESTCID-DFIF_DFDF','TESTS/PATH1', { testkey:'testvalue'}, null, true).then(app.log, app.failure);
-	},
-	searchDocuments: function() {
-		app.clear();
-		cordova.plugins.DCSync.searchDocuments({}, {path:document.getElementById('search').value}).then(app.log, app.failure);
-	},
-	getDocumentCount: function() {
-		app.clear();
-		cordova.plugins.DCSync.getDocumentCount(document.getElementById('search').value).then(app.log, app.failure);
-	},
-	deleteDocument: function() {
-		app.clear();
-		cordova.plugins.DCSync.deleteDocument('TESTCID-DFIF_DFDF').then(app.log, app.failure);
-	},
-
-	setSyncOptions: function() {
-		app.clear();
-		cordova.plugins.DCSync.setSyncOptions({url:'https://media.kju.at/datacollector', username:'anonymous', password:'none', interval: 1440}).then(app.log, app.failure);
-	},
-
-	setSyncOptions: function() {
-		app.clear();
-		cordova.plugins.DCSync.setSyncOptions({url:'http://10.0.2.2:49341/DC', username:'anonymous', password:'YTI2Y2UxYzgxZTBiN2U4OWZmZjU1OWJmYmU4ZTEwN2E2MzFhNGFlMmFkYzlhMWMxYmE1YWYyOGNiYWMyZWI4ZA==', interval: 1440}).then(app.log, app.failure);
-	}
 	
 	
 }
